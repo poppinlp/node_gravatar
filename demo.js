ef8a341da7642c3969cf7ec76fcf3e973238de70
PoppinL <poppinlp@gmail.com>
2013-11-05 10:18:44 +0800

    add getProfile

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
 var gravatar = require('./gravatar.js');
 
 // http
-console.log(gravatar.getUrl('poppinlp@gmail.com'));
+console.log(gravatar.getAvatar('poppinlp@gmail.com'));
 // params
 // for more details please visit http://en.gravatar.com/site/implement/images/
-console.log(gravatar.getUrl('poppinlp@gmail.com', {s: 100, d: 'retro'}));
+console.log(gravatar.getAvatar('poppinlp@gmail.com', {s: 100, d: 'retro'}));
 // https
-console.log(gravatar.getUrl('poppinlp@gmail.com', {}, 1));
+console.log(gravatar.getAvatar('poppinlp@gmail.com', {}, 1));
--- gravatar.js
+++ gravatar.js
@@ -1,10 +1,15 @@
 var crypto = require('crypto'),
     qs = require('querystring'),
     gravatar = module.exports = {
-        getUrl: function (email, params, https) {
+        getAvatar: function (email, params, https) {
             var baseUrl = (https && 'https://secure.gravatar.com/avatar/') || 'http://www.gravatar.com/avatar/',
                 md5 = crypto.createHash('md5').update(email.toLowerCase().trim()).digest('hex'),
                 query = (params && '?' + qs.stringify(params)) || '';
             return baseUrl + md5 + query;
+        },
+        getProfile: function (email, https) {
+            var baseUrl = (https && 'https://secure.gravatar.com/') || 'http://www.gravatar.com/',
+                md5 = crypto.createHash('md5').update(email.toLowerCase().trim()).digest('hex');
+            return baseUrl + md5 + '.json';
         }
     };
